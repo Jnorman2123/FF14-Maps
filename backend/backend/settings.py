f193796c298892d055577d58173f5d1b74471d62
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-17 18:40:18 -0500

    Created apps for each of items, npcs, steps, and rewards
    Created apps for each of items, npcs, steps, and rewards

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,11 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quests.apps.QuestsConfig',
-    'npcs.apps.NpcsConfig',
-    'items.apps.ItemsConfig',
-    'rewards.apps.RewardsConfig',
-    'steps.apps.StepsConfig',
-    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
