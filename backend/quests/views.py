1bd0f23fdb1491acced6df56689ba159493a26a0
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-17 18:46:37 -0500

    Created basic views for each of the models
    Created basic views for each of the models

--- backend/quests/views.py
+++ backend/quests/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from .models import Quest, Item, Npc, Reward, Step
+from .serializers import QuestSerializer, ItemSerializer, NpcSerializer, RewardSerializer, StepSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+
+class QuestListCreate(generics.ListCreateAPIView):
+    queryset = Quest.objects.all()
+    serializer_class = QuestSerializer
+
+
+class ItemListCreate(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class NpcListCreate(generics.ListCreateAPIView):
+    queryset = Npc.objects.all()
+    serializer_class = NpcSerializer
+
+
+class RewardListCreate(generics.ListCreateAPIView):
+    queryset = Reward.objects.all()
+    serializer_class = RewardSerializer
+
+
+class StepListCreate(generics.ListCreateAPIView):
+    queryset = Step.objects.all()
+    serializer_class = StepSerializer
