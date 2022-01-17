814d0448fc1fb211c7a387cac54bd36643664a96
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-17 18:47:35 -0500

    Added url patterns for each app to urls.py
    Added url patterns for each app to urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('quests.urls')),
+    path('', include('items.urls')),
+    path('', include('npcs.urls')),
+    path('', include('steps.urls')),
+    path('', include('rewards.urls')),
 ]
