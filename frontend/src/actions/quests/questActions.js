6a20eba6ca025460218edbf9ec59667097905e86
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-18 10:58:36 -0500

    Create questActions.js
    Added a get request for quest actions

---
+++ frontend/src/actions/quests/questActions.js
@@ -0,0 +1,10 @@
+export function fetchQuests() {
+    return (dispatch) => {
+        dispatch({type: "START_LOADING_QUESTS_REQUEST"});
+        fetch("http://127.0.0.1:8000/api/quests/")
+            .then((resp) => resp.json())
+            .then((quests) => 
+                dispatch({type: "LOAD_QUESTS", quests})
+            );
+    };
+}
