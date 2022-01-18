ad5edc7b4ebfe6aa980fbef3a059828d53ed04ef
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-18 12:13:25 -0500

    Update index.js
    Added variable for store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import configureStore, { history } from './configureStore';
 import 'bootstrap/dist/css/bootstrap.css';
 
+const store = configureStore();
+
 ReactDOM.render(
   <Provider store={store}>
     <App history={history} />
