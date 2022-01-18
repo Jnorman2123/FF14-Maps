b503a5f874ec3435b1a1ac2aa35ed027795b90d0
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-18 10:37:12 -0500

    Added configurestore file to the frontend
    Added configurestore file to the frontend

---
+++ frontend/src/configureStore.js
@@ -0,0 +1,19 @@
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { routerMiddleware } from 'connected-react-router';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+
+export const history = createBrowserHistory();
+
+export default function configureStore(preLoadedState) {
+    const composeEnhancer = 
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+    const store = createStore(
+        rootReducer(history),
+        preLoadedState,
+        composeEnhancer(applyMiddleware(routerMiddleware(history), thunk))
+    );
+    return store;
+}
