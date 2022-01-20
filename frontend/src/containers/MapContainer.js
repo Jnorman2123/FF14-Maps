46d5cf99cbdd3f9a5bdf32ec0f5321bbfd1feb36
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-20 17:04:35 -0500

    Create MapContainer.js
    Created basic mapcontainer component

---
+++ frontend/src/containers/MapContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+    render() {
+        return <h1 className='text-center'>Map {this.props.mapName} </h1>
+    }
+}
+
+export default MapContainer;
