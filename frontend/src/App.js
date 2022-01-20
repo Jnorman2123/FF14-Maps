6032448ec255c62a715dded5f9bdacb7a4c8ddde
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-20 12:52:59 -0500

    Update App.js
    Changed app to be a react component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,43 +7,34 @@ import ToggleContainer from './containers/ToggleContainer';
 import WorldNav from './containers/WorldNav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div>
-      <Container fluid>
-        <Row>
-          <WorldNav />
-        </Row>
-        <Row>
-          <Col md={3} className='bg-dark'>
-            <ToggleContainer />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          </Col>
-          <Col md={{span: 3, offset: 6}} className='bg-dark'>
-            <QuestContainer />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Container fluid>
+          <Row>
+            <WorldNav />
+          </Row>
+          <Row>
+            <Col md={3} className='bg-dark'>
+              <ToggleContainer />         
+            </Col>
+            <Col md={{span: 3, offset: 6}} className='bg-dark'>
+              <QuestContainer />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  } 
 }
 
 export default App;
