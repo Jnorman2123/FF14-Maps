d5767daeee7dbaa4ffaf487497e6ea22d974dee3
Jonathan Norman <Jnorman2123@gmail.com>
2022-01-28 16:03:06 -0500

    Update MapCont.js
    The map container now displays the active quest steps on the map

--- frontend/src/containers/MapCont.js
+++ frontend/src/containers/MapCont.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { MapContainer, Marker, Popup, ImageOverlay } from 'react-leaflet';
 import L from 'leaflet';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import { connect } from 'react-redux';
 
 class MapCont extends Component {
@@ -25,6 +22,7 @@ class MapCont extends Component {
                 })
                 return active_quests;
             }
+            return active_quests;
         })
         console.log(active_quests);
         let npcs = this.props.npcs.npcs.filter(npc => npc.npc_zone.includes(this.props.mapName))
@@ -32,45 +30,29 @@ class MapCont extends Component {
 
             <div>
                 <div className='text-center' >{this.props.mapName} </div>
-                <Container>
-                    <Row>
-                        <Col>
-                            <ul>
-                                {active_quests.map(quest => {
-                                    let quest_steps = [];
-                                    this.props.steps.steps.filter(step => step.quest_step === quest.id).map(s => {
-                                        quest_steps.push(s);
-                                        return quest_steps;
-                                    })
-                                    return <li key={quest.quest_name}>
-                                        {quest.quest_name}-{quest.quest_type}-{quest.quest_class}-{quest.quest_level}
-                                        <ul>
-                                            {quest_steps.map(step => {
-                                                let step_npc = npcs.filter(npc => npc.id === step.step_npc);
-                                                if (step_npc[0] !== undefined) {
-                                                    return <li key={step.step_description}>{step.step_description}<p>{step_npc[0].npc_name}</p></li>
-                                                }
-                                            })}
-                                        </ul>
-                                    </li>
-                                })}
-                            </ul>
-                        </Col>
-                    </Row>
-                </Container>
-                
-                {/* <MapContainer key={Math.random()} crs={L.CRS.Simple} center={this.props.center} zoom={this.props.zoom} minZoom={this.props.minZoom}
+                <MapContainer key={Math.random()} crs={L.CRS.Simple} center={this.props.center} zoom={this.props.zoom} minZoom={this.props.minZoom}
                 maxZoom={this.props.maxZoom} maxBounds={this.props.bounds} maxBoundsViscosity='1' scrollWheelZoom={true}
                 style={{height: '800px', width: '935px'}}>
                     <ImageOverlay url='./maps/NewGridania_Base_v3.jpg' bounds={this.props.bounds} opacity={1} />
-                    {this.props.npcs.npcs.filter(npc => npc.npc_zone.includes(this.props.mapName)).map(n => {
-                        return <Marker key={n.npc_name} position={this.props.revertLat(n.npc_location_x, n.npc_location_y)}>
-                            <Popup>
-                                {n.npc_name}
-                            </Popup>
-                        </Marker>
+                    {active_quests.map(quest => {
+                        let quest_steps = [];
+                        this.props.steps.steps.filter(step => step.quest_step === quest.id).map(s => {
+                            quest_steps.push(s);
+                            return quest_steps;
+                        })
+                        return quest_steps.map(step => {
+                            let step_npc = npcs.filter(npc => npc.id === step.step_npc);
+                            if (step_npc[0] !== undefined) {
+                                return <Marker key={Math.random()} 
+                                position={this.props.revertLat(step_npc[0].npc_location_x, step_npc[0].npc_location_y)}>
+                                    <Popup>
+                                        {step_npc[0].npc_name}
+                                    </Popup>
+                                </Marker>
+                            }
+                        });
                     })}
-                </MapContainer> */}
+                </MapContainer>
             </div>
             
         );
