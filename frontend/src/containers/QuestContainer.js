ec699f785c61a1858ea95245ef7d5016c4193c9d
Jonathan Norman <Jnorman2123@gmail.com>
2022-02-03 18:26:30 -0500

    Update QuestContainer.js
    The quest container now displays every step for the selected quest

--- frontend/src/containers/QuestContainer.js
+++ frontend/src/containers/QuestContainer.js
@@ -7,11 +7,15 @@ class QuestContainer extends Component {
 
     renderQuestInfo = (active_quest) => {
         let quest_reward = null;
+        let quest_steps = [];
         if (active_quest.length > 0) {
             quest_reward = this.props.rewards.rewards.filter(r => r.id === active_quest[0].quest_reward);
+            quest_steps = this.props.steps.steps.filter(s => s.quest_step === active_quest[0].id);
+            console.log(quest_steps);
+
             return <Container key={Math.random()} className='bg-primary' >
-                <Row><h1>{active_quest[0].quest_name}</h1></Row>
-                <Row><h2>Quest Reward</h2></Row>
+                <Row><h1 className='text-center'>{active_quest[0].quest_name}</h1></Row>
+                <Row><h2 className='text-center'>Quest Reward</h2></Row>
                 <Row>
                     <Col md={6} >
                         <h4>Experience: {quest_reward[0].reward_experience}</h4>
@@ -19,16 +23,25 @@ class QuestContainer extends Component {
                     </Col>
                     <Col md={6} >
                         <h4>
-                            Item Rewards: <ol>
+                            Item Rewards: <ul>
                                 {quest_reward[0].reward_items.map(ri => {
                                     let item = this.props.items.items.filter(i => i.id === ri);
-                                    return <li key={Math.random()}><h6>{item[0].item_name}: {item[0].item_quantity}</h6></li>
+                                    return <li key={ri}><h6>{item[0].item_name}: {item[0].item_quantity}</h6></li>
                                 })}
-                            </ol>
+                            </ul>
                         </h4>
                     </Col>
                 </Row>
-                <Row><h4>Other: None</h4></Row>
+                <Row><h4>Other: {quest_reward[0].reward_other}</h4></Row>
+                <Row className='text-center'><h2>Quest Steps</h2></Row>
+                <Row>
+                    <ol>
+                        {quest_steps.map(s => {
+                            let npc = this.props.npcs.npcs.filter(n => n.id === s.step_npc)
+                            return <li key={s.step_description}><h6>{s.step_description}-{npc[0].npc_name}-{npc[0].npc_zone}</h6></li>
+                        })}
+                    </ol>
+                </Row>
             </Container>
         } else {
             return <Container key={Math.random()} className='bg-primary' >
