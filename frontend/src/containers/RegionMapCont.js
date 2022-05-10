bc56f4d3b4a10a1b5e0014aa0a23be1043239b46
Jonathan Norman <Jnorman2123@gmail.com>
2022-05-10 14:01:59 -0400

    Update RegionMapCont.js
    Added polygon around western la noscea

--- frontend/src/containers/RegionMapCont.js
+++ frontend/src/containers/RegionMapCont.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MapContainer, Marker, Popup, ImageOverlay, LayerGroup } from 'react-leaflet';
+import { MapContainer, Marker, Popup, ImageOverlay, LayerGroup, Polygon } from 'react-leaflet';
 import L from 'leaflet';
 import { connect } from 'react-redux';
 import Button from 'react-bootstrap/esm/Button';
@@ -18,6 +18,14 @@ class RegionMapCont extends Component {
         let lvl_ranges = lvls.map(l => l.split("-").map(i => parseInt(i)));
         let in_zone_quests = [];
         let active_in_zone_quests = [];
+        let westernLaNoscea = [
+            this.props.revertLat(8.2, 15.1), this.props.revertLat(7.7, 13.5), this.props.revertLat(9.4, 12.9), 
+            this.props.revertLat(11.2, 14.0), this.props.revertLat(13.3, 16.0), this.props.revertLat(15.9, 16.6), 
+            this.props.revertLat(17.3, 18.9), this.props.revertLat(19.1, 19.4), this.props.revertLat(19.5, 20.0),
+            this.props.revertLat(17.9, 21.8), this.props.revertLat(15.9, 20.9), this.props.revertLat(13.8, 19.6),
+            this.props.revertLat(13.0, 18.4), this.props.revertLat(10.2, 17.6), this.props.revertLat(9.9, 15.5),
+        ]
+        let purpleOptions = { color: 'purple'}
 
         return (
             <Container>
@@ -26,6 +34,7 @@ class RegionMapCont extends Component {
                     minZoom={this.props.minZoom} maxZoom={this.props.maxZoom} maxBounds={this.props.bounds} 
                     maxBoundsViscosity='1' scrollWheelZoom={true} style={{height: '800px', width: '935px'}}>
                     <ImageOverlay url={`./maps/${mapName}.png`} bounds={this.props.bounds} opacity={1} />
+                    <Polygon positions={westernLaNoscea} pathOptions={purpleOptions} />
                 </MapContainer>
             </Container>
         )
