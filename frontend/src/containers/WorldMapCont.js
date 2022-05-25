d59de15b1498fd441740c7fb7ae80b45a7dcbcf1
Jonathan Norman <Jnorman2123@gmail.com>
2022-05-25 13:57:51 -0400

    The regions now light up when the mouse is over them
    The regions now light up when the mouse is over them

--- frontend/src/containers/WorldMapCont.js
+++ frontend/src/containers/WorldMapCont.js
@@ -62,8 +62,18 @@ class WorldMapCont extends Component {
                 position: [-40.15, 39.1]
                 }
             ],
+            highlightedMarkers: [],
         }
     };
+
+    addHighlightedMarker = (pos, icon) => {
+        let marker = {icon: icon, position: pos};
+        this.setState({ highlightedMarkers: [...this.state.highlightedMarkers, marker]})
+    }
+
+    removeHighlightedMarker = () => {
+        this.setState({highlightedMarkers: []});
+    }
 
 
     render () {
@@ -79,21 +89,65 @@ class WorldMapCont extends Component {
             [-25, 26], [-21, 22], [-16, 22], [-14, 27], [-14, 30], [-16, 32], [-20, 32], [-24, 31]
         ];
         let polyOptions = { color: 'purple' }
+        let laNosceaIcon = new L.Icon({
+            iconUrl: `./maps/LaNosceaHighlighted.png`,
+            iconRetinaUrl: `./maps/LaNosceaHighlighted.png`,
+            popupAnchor: [0, 0],
+            iconSize: [775.775, 773.45],
+        });
+        let thanalanIcon = new L.Icon({
+            iconUrl: `./maps/ThanalanHighlighted.png`,
+            iconRetinaUrl: `./maps/ThanalanHighlighted.png`,
+            popupAnchor: [0, 0],
+            iconSize: [775.775, 773.45],
+        });
+        let theBlackShroudIcon = new L.Icon({
+            iconUrl: `./maps/TheBlackShroudHighlighted.png`,
+            iconRetinaUrl: `./maps/TheBlackShroudHighlighted.png`,
+            popupAnchor: [0, 0],
+            iconSize: [775.775, 773.45],
+        });
+        let highlightPos = [-21.5, 21.4];
 
         return (
             <Container>
                 <MapContainer maxBounds={this.props.bounds} center={this.props.center} zoom={this.props.zoom}
                 crs={L.CRS.Simple} maxBoundsViscosity='1' scrollWheelZoom={false} maxZoom={this.props.zoom}
                 minZoom={this.props.zoom} style={{height: '800px', width: '900px'}} >
-                    <Polygon positions={laNoscea} pathOptions={polyOptions} opacity={1} />
-                    <Polygon positions={thanalan} pathOptions={polyOptions} opacity={1} />
-                    <Polygon positions={theBlackShroud} pathOptions={polyOptions} opacity={1} />
+                    <Polygon positions={laNoscea} pathOptions={polyOptions} opacity={1} eventHandlers={{
+                        mouseover: () => {
+                            this.addHighlightedMarker(highlightPos, laNosceaIcon);
+                        },
+                        mouseout: () => {
+                            this.removeHighlightedMarker();
+                        }
+                    }} />
+                    <Polygon positions={thanalan} pathOptions={polyOptions} opacity={1} eventHandlers={{
+                        mouseover: () => {
+                            this.addHighlightedMarker(highlightPos, thanalanIcon);
+                        },
+                        mouseout: () => {
+                            this.removeHighlightedMarker();
+                        }
+                    }}/>
+                    <Polygon positions={theBlackShroud} pathOptions={polyOptions} opacity={1} eventHandlers={{
+                        mouseover: () => {
+                            this.addHighlightedMarker(highlightPos, theBlackShroudIcon);
+                        },
+                        mouseout: () => {
+                            this.removeHighlightedMarker();
+                        }
+                    }}/>
                     <ImageOverlay url={`./maps/${this.props.mapName}.png`} bounds={this.props.bounds} opacity={1} />
                     {this.state.keyMarkers.map(mar => {
                         return <Marker key={Math.random()} icon={mar.icon} position={mar.position} zIndexOffset={250}/>
                     })}
                     {this.state.zoneMarkers.map(mar => {
                         return <Marker key={Math.random()} icon={mar.icon} position={mar.position} zIndexOffset={500}/>
+                    })}
+                    {this.state.highlightedMarkers.map(mar => {
+                        return <Marker key={Math.random()} icon={mar.icon} position={mar.position} zIndexOffset={0} 
+                        opacity={.5} interactive={false} />
                     })}
                 </MapContainer>
             </Container>
