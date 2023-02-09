e35005da5554d107c7c24ea65f75ed240b4beb0a
Jonathan Norman <Jnorman2123@gmail.com>
2023-02-09 11:56:17 -0500

    Added a hover state to the world map button and added a link the world page to the button
    Added a hover state to the world map button and added a link the world page to the button

--- frontend/src/containers/Info.js
+++ frontend/src/containers/Info.js
@@ -4,9 +4,24 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/esm/Col';
 import Image from 'react-bootstrap/esm/Image';
+import OverlayTrigger from 'react-bootstrap/esm/OverlayTrigger';
+import Tooltip from 'react-bootstrap/esm/Tooltip';
 
 class Info extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hovered: false,
+        }
+    }
+
     render() {
+        let world_map_button = ' ';
+
+        this.state.hovered ? world_map_button = '../home_page/WorldButtonHover.jpg' 
+        : world_map_button = '../home_page/WorldButton.jpg';
+
         return (
             <Container style={{padding: 0}}>
                 <Row>
@@ -17,7 +32,19 @@ class Info extends Component {
                         <Image style={{height: 175, width: '100%'}} src='../home_page/HomePageTriston.jpg' />
                     </Col>
                     <Col md={2} className='bg-warning' style={{padding: 0}}>
-                        <Image style={{height: 175, width: '100%'}} src='../home_page/WorldButton.jpg' />
+                        <OverlayTrigger placement='left' overlay={<Tooltip id="button-tooltip-2" >
+                            Navigate to World Map
+                            </Tooltip>} >
+                            <Link to='/world'
+                            onMouseEnter={() => {
+                                this.setState({hovered: true});
+                            }}
+                            onMouseLeave={() => {
+                                this.setState({hovered: false});
+                            }}>
+                                <Image src={world_map_button} style={{height: 175, width: '100%'}} name='world' />
+                            </Link>
+                        </OverlayTrigger>
                     </Col>
                     <Col style={{paddingLeft: 0, paddingTop: 0, paddingRight: 12}} className='bg-danger'>
                         <Image style={{height: 175, width: '100%'}} src='../home_page/HomePageEadin.jpg' />
