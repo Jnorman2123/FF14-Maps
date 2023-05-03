484e9e198d55118a9ed4a034415eb2e85c7e0345
Jonathan Norman <Jnorman2123@gmail.com>
2023-05-03 11:44:51 -0400

    Added the navbar background image
    Added the navbar background image

--- frontend/components/navBar.tsx
+++ frontend/components/navBar.tsx
@@ -19,18 +19,22 @@ export default function Example() {
     let thanalanZoneNames = useSelector(getThanalanZoneNamesState);
 
   return (
-    <div>
+    <div className='h-navbar relative bg-center bg-cover' style={{ backgroundImage: "url('/nav_bar/NavBar.jpg')", height: '140px'}}>
         {regionNames.map((regionName: string) => {
             let zones = [];
+            let divClassName = '';
             let regionLink = regionName.split(' ').join('-');
             if (regionName === 'La Noscea') {
                 zones = laNosceaZoneNames;
+                divClassName = "absolute bottom-0 left-10 inline-block text-left";
             } else if (regionName === 'The Black Shroud') {
                 zones = theBlackShroudZoneNames;
+                divClassName = "absolute bottom-0 left-40 inline-block text-left";
             } else {
                 zones = thanalanZoneNames;
+                divClassName = "absolute bottom-0 left-80 inline-block text-left";
             }
-            return <Menu key={regionName} as="div" className="relative inline-block text-left">
+            return <Menu key={regionName} as="div" className={divClassName}>
                 <div>
                     <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm 
                     font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
@@ -47,7 +51,7 @@ export default function Example() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                 >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1
+                    <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1
                      ring-black ring-opacity-5 focus:outline-none">
                         <div className="py-1">
                             <Menu.Item>
