6a6d044e6d8524e637df9eed7fd7a4358c8989fd
Jonathan Norman <Jnorman2123@gmail.com>
2023-05-08 10:57:56 -0400

    Changed the color and opacity of the nav bar tabs
    Changed the color and opacity of the nav bar tabs

--- frontend/components/navBar.tsx
+++ frontend/components/navBar.tsx
@@ -37,8 +37,8 @@ export default function NavBar() {
                 }
                 return <div className='grid-cols-1 relative'>
                     <Menu key={regionName} as="div" className="text-left absolute bottom-0 w-full">
-                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-lg bg-white px-3 
-                        py-2 text-navtab font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-lg bg-gray-600 px-3 
+                        py-2 text-navtab font-bold text-white shadow-sm hover:bg-gray-700 opacity-75">
                             {regionName}
                             <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
                         </Menu.Button>
